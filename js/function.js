85afe4c7e73fd824d37027ab7b95113af2d97dd7
samandargofurov <samandargofurov236@gmail.com>
2024-03-03 18:41:47 +0500

    5-oy imtihon

---
+++ js/function.js
@@ -0,0 +1,16 @@
+function createCard(country) {
+    return `
+            <div class="card" style="height: 336px;">
+                <img src="${country.flags.png}" alt="Flag picture">
+
+                <div class="info">
+                    <h4 id="name">${country.name.common}</h4>
+                    <p><span>Population:</span> ${country.population.toLocaleString("en-IN")}</p>
+                    <p><span>Region:</span> ${country.region}</p>
+                    <p><span>Capital:</span> ${country.capital}</p>
+                </div>
+            </div>
+        `;
+}
+
+export { createCard };
---
+++ js/index.js
@@ -0,0 +1,42 @@
+import { createCard } from "./function.js";
+
+const icon = document.querySelector('#icon');
+const search = document.querySelector('#search');
+const select = document.getElementById('select');
+const cards = document.querySelector('#cards');
+
+window.addEventListener('load', function() {
+    const loader = document.querySelector('.loader');
+
+    loader.classList.add("loader-hidden");
+    loader.addEventListener('transitionend', function () {
+        document.body.removeChild("loader")
+    })
+})
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch("https://frontend-mentor-apis-6efy.onrender.com/countries", {
+        method: "GET"
+    })
+        .then(res => {
+                return res.json();
+        })
+        .then(data => {
+            if (data) {
+                data.data.forEach(country => {
+                    let row = createCard(country);
+                    cards.innerHTML += row
+                });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+});
+
+icon && icon.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    
+})
